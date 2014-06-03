dfd42af76c182169718d219d45ac4ea8ccdfeaf5
ael-code <tommy.ael@gmail.com>
2014-06-03 15:27:29 +0200

    jax testing

--- web_gui/test_engine.php
+++ web_gui/test_engine.php
@@ -1,9 +1,4 @@
 <?php
-	if($_SERVER['REQUEST_METHOD'] == 'POST'){
-	echo "no post request";
-	}else{
-	echo "post request";}
-	var_dump($_POST);
 	require "engine.php";
-	var_dump(get_info($_POST["uri"],$_POST["ip"]));
+	print(get_info($_POST["uri"],$_POST["ip"]));
 ?>
