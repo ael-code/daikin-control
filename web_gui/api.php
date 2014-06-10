1e92f4ac6278eff6f49ffdfb8bd9b66d81c5780c
ael-code <tommy.ael@gmail.com>
2014-06-10 14:33:29 +0200

    fixed error in api control

--- web_gui/api.php
+++ web_gui/api.php
@@ -3,7 +3,7 @@
 	require "config.php";
 	
 	//control if uri is sended
-	if(! in_array("uri", $_GET)){
+	if(! isset($_GET["uri"])){
 		http_response_code(405); //method not allowed
 		exit;
 	}
@@ -14,7 +14,7 @@
 	}else if($_GET["uri"] == "/aircon/get_sensor_info" || $_GET["uri"] == "/aircon/get_control_info"){
 		$json_info=get_json_info($_GET["uri"],$ip);
 		//request failed
-		if($json _info===FALSE){
+		if($json_info===FALSE){
 			http_response_code(503); //service Unavailable 
 			exit;
 		}
